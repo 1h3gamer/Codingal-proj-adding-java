9f34e2bc40cb52a190cee4c0d0ef9d80764e4002
1h3gamer <Idaune.nathan@gmail.com>
2026-09-19 08:47:50 +0100

    adding int proj

---
+++ main.java
@@ -0,0 +1,11 @@
+class Main {
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 20;
+
+        int z = x + y;
+
+        // Step 3: Display the value of z
+        System.out.println("The sum is: " + z);
+    }
+}
